0efe6fb9af29758b481761c158056d72893c5c0e
windows0xsupport <ankitkatariacool9@gmail.com>
2026-01-08 23:01:21 +0530

    changes

--- index.php
+++ index.php
@@ -373,15 +373,7 @@ visibility: hidden;
 
   gtag('config', 'UA-153548451-1');
 </script>
-<script type="text/javascript">
-  window._mfq = window._mfq || [];
-  (function() {
-    var mf = document.createElement("script");
-    mf.type = "text/javascript"; mf.defer = true;
-    mf.src = "//cdn.mouseflow.com/projects/6c293c4f-8b14-4a19-bbc5-c2108003ae3f.js";
-    document.getElementsByTagName("head")[0].appendChild(mf);
-  })();
-</script>
+<script async defer src="https://tools.luckyorange.com/core/lo.js?site-id=38399c18"></script>
 
 <script>
   var t = new XMLHttpRequest;
